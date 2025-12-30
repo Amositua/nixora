2a20c62a4a66935bbaeb45221035e4e9a50d3df4
Amos omohodion <amosomohodion@gmail.com>
2025-12-30 19:49:40 +0100

    implemented loan report feature

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -12,6 +12,7 @@ import {
   Menu,
   LogOut,
 } from "lucide-react";
+import Logo from "../../assets/sa.jpeg";
 
 // const user = localStorage.getItem("user");
 // console.log("Layout user:", user);
@@ -49,9 +50,10 @@ export default function Layout({
       <aside className="w-64 bg-white border-r border-gray-200 flex flex-col">
         <div className="h-16 flex items-center px-6 border-b border-gray-200">
           <div className="flex items-center space-x-2">
-            <div className="w-8 h-8 bg-gradient-to-br from-blue-500 to-teal-500 rounded-lg flex items-center justify-center">
-              <FileText className="w-5 h-5 text-white" />
-            </div>
+            {/* <div className="w-8 h-8 bg-gradient-to-br from-blue-500 to-teal-500 rounded-lg flex items-center justify-center"> */}
+              {/* <FileText className="w-5 h-5 text-white" /> */}
+            {/* </div> */}
+            <img src={Logo} alt="Logo" className="w-8 h-8 rounded-lg" />
             <span className="text-lg font-semibold text-gray-900">Nixora</span>
           </div>
         </div>
@@ -86,7 +88,7 @@ export default function Layout({
             onClick={() => setShowUserMenu(!showUserMenu)}
             className="w-full flex items-center space-x-3 px-3 py-2 rounded-lg hover:bg-gray-50 transition-colors"
           >
-            <div className="w-8 h-8 bg-gradient-to-br from-blue-600 to-teal-600 rounded-full flex items-center justify-center text-white text-sm font-medium">
+            <div className="w-8 h-8 bg-gradient-to-br from-blue-400 to-teal-600 rounded-full flex items-center justify-center text-white text-sm font-medium">
               {user
                 ? JSON.parse(user)
                     .name.split(" ")
@@ -170,7 +172,7 @@ export default function Layout({
                 setCurrentScreen("settings");
               }}
             >
-              <div className="w-9 h-9 bg-gradient-to-br from-blue-600 to-teal-600 rounded-full flex items-center justify-center text-white text-sm font-medium cursor-pointer">
+              <div className="w-9 h-9 bg-gradient-to-br from-blue-400 to-teal-600 rounded-full flex items-center justify-center text-white text-sm font-medium cursor-pointer">
                 {user
                   ? JSON.parse(user)
                       .name.split(" ")
